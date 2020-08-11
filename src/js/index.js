6ea7f903efaecba8187ce4405fddfcdf5a6878c6
Evgeny Leukhin <eugenel@effective.band>
2020-08-11 17:16:02 +0600

    Footer left

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,37 @@ import '../scss/styles.scss';
 
 // jq-code
 $(document).ready(() => {
-  // jq code
+  const burgerBtn = $(".js-burger-btn");
+  const mobileMenu = $(".js-mobile-menu");
+  const mobileSubmenuLink = $(".js-mobile-submenu-link");
+
+  // show mobile-menu
+  burgerBtn.on("click", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    burgerBtn.toggleClass("close");
+    mobileMenu.toggleClass("show");
+    $('body').toggleClass("js-overlay");
+  });
+
+  // show mobile-submenu
+  mobileSubmenuLink.on("click", function(e) {
+    e.preventDefault();
+
+    $(this).toggleClass("open");
+  });
+
+  // click outside handler when mobile menu is open
+  $(document).click(function () {
+    burgerBtn.removeClass("close");
+    mobileMenu.removeClass("show");
+    $("body").removeClass("js-overlay");
+  });
+
+  // stopPropagation inside mobileMenu
+  mobileMenu.click(function(e) {
+    e.stopPropagation();
+  });
 });
 
